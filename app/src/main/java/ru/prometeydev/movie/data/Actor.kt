1c6f913ff3c73c23631df9c271a72421bb2a1992
Ruslan Sabirov <ruslan.sabirov@hotmail.com>
2020-12-08 21:00:17 +0300

    Add prepared asset and data serialization

---
+++ app/src/main/java/ru/prometeydev/movie/data/Actor.kt
@@ -0,0 +1,7 @@
+package ru.prometeydev.movie.data
+
+data class Actor(
+    val id: Int,
+    val name: String,
+    val picture: String
+)
---
+++ app/src/main/java/ru/prometeydev/movie/data/Genre.kt
@@ -0,0 +1,6 @@
+package ru.prometeydev.movie.data
+
+data class Genre(
+    val id: Int,
+    val name: String
+)
