ced3035932a196b94669620234b117286ba5235a
ruslan.sabirov <ruslan.sabirov@hotmail.com>
2021-02-12 18:03:12 +0300

    add picture into notification

--- app/src/main/java/ru/prometeydev/movie/ui/MovieNotifications.kt
+++ app/src/main/java/ru/prometeydev/movie/ui/MovieNotifications.kt
@@ -1,19 +1,33 @@
 package ru.prometeydev.movie.ui
 
-import android.app.Notification
 import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.widget.RemoteViews
+import androidx.annotation.IdRes
 import androidx.annotation.WorkerThread
 import androidx.core.app.*
 import androidx.core.app.NotificationManagerCompat.IMPORTANCE_HIGH
-import androidx.core.graphics.drawable.IconCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.net.toUri
+import coil.ImageLoader
+import coil.annotation.ExperimentalCoilApi
+import coil.request.ImageRequest
+import coil.target.Target
 import ru.prometeydev.movie.R
 import ru.prometeydev.movie.model.domain.Movie
 
+
+
 class MovieNotifications(private val context: Context) {
+
+    private val imageLoader = ImageLoader.Builder(context)
+            .availableMemoryPercentage(0.25)
+            .crossfade(true)
+            .build()
 
     private val notificationManagerCompat = NotificationManagerCompat.from(context)
 
@@ -28,31 +42,34 @@ class MovieNotifications(private val context: Context) {
         }
     }
 
+    @ExperimentalCoilApi
     @WorkerThread
-    fun show(movie: Movie) {
-        val icon = IconCompat.createWithContentUri(movie.poster)
+    suspend fun show(movie: Movie) {
         val contentUri = "https://movie.prometeydev.ru/movie/${movie.id}".toUri()
-        val person = Person.Builder()
-                .setName(movie.title)
-                .setIcon(icon)
-                .build()
 
-        /*val small = RemoteViews(context.packageName, R.layout.notification_small).apply {
+        val smallContentView = RemoteViews(context.packageName, R.layout.notification_small).apply {
+            setTextViewText(R.id.appears, context.getString(R.string.chanel_new_top_rated_movie_description))
             setTextViewText(R.id.movie_name, movie.title)
+            setTextViewText(R.id.rating, context.getString(R.string.rating_text, movie.ratings.toString()))
         }
 
-        val big = RemoteViews(context.packageName, R.layout.notification_big).apply {
+        val bigContentView = RemoteViews(context.packageName, R.layout.notification_big).apply {
+            setTextViewText(R.id.appears, context.getString(R.string.chanel_new_top_rated_movie_description))
             setTextViewText(R.id.movie_name, movie.title)
-        }*/
+            setTextViewText(R.id.rating, context.getString(R.string.rating_text, movie.ratings.toString()))
+            setTextViewText(R.id.genres, movie.genres.joinToString { it.name })
+            setTextViewText(R.id.overview, movie.overview)
+        }
+
+        loadImage(smallContentView, R.id.movie_image, movie.poster)
+        loadImage(bigContentView, R.id.movie_image, movie.poster)
 
         val notification = NotificationCompat.Builder(context, NOTIFY_CHANNEL_ID)
-                .setContentTitle(movie.title)
-                .setContentText(context.getString(R.string.new_top_rated_movie_with_rating, movie.ratings.toString()))
                 .setSmallIcon(R.drawable.ic_movie_notify)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setOnlyAlertOnce(true)
-                //.setCustomContentView(small)
-                //.setCustomBigContentView(big)
+                .setCustomContentView(smallContentView)
+                .setCustomBigContentView(bigContentView)
                 .setContentIntent(
                     PendingIntent.getActivity(
                             context,
@@ -63,7 +80,6 @@ class MovieNotifications(private val context: Context) {
                             PendingIntent.FLAG_UPDATE_CURRENT
                     )
                 )
-                //.setStyle(NotificationCompat.MessagingStyle(person))
                 .setWhen(System.currentTimeMillis())
                 .build()
 
@@ -74,10 +90,39 @@ class MovieNotifications(private val context: Context) {
         notificationManagerCompat.cancel(NOTIFY_TAG, movieId)
     }
 
+    @ExperimentalCoilApi
+    private suspend fun loadImage(remoteViews: RemoteViews, resId: Int, uri: String?) {
+        val target = RemoteViewsTarget(context, ComponentName(context.packageName, "MainActivity"), remoteViews, resId)
+        val request = ImageRequest.Builder(context)
+                .data(uri)
+                .target(target)
+                .build()
+        imageLoader.enqueue(request).await()
+    }
+
     companion object {
         private const val NOTIFY_TAG = "movie"
         private const val NOTIFY_CHANNEL_ID = "NOTIFY_CHANNEL"
         private const val REQUEST_CONTENT = 1
+    }
+
+    private class RemoteViewsTarget(
+            private val context: Context,
+            private val componentName: ComponentName,
+            private val remoteViews: RemoteViews,
+            @IdRes private val imageViewResId: Int
+    ) : Target {
+
+        override fun onStart(placeholder: Drawable?) = setDrawable(placeholder)
+
+        override fun onError(error: Drawable?) = setDrawable(error)
+
+        override fun onSuccess(result: Drawable) = setDrawable(result)
+
+        private fun setDrawable(drawable: Drawable?) {
+            remoteViews.setImageViewBitmap(imageViewResId, drawable?.toBitmap())
+            AppWidgetManager.getInstance(context).updateAppWidget(componentName, remoteViews)
+        }
     }
 
 }
